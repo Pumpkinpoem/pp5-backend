b83d328c31dfba259f4120d308082ceb15428a67
Andreas Almen <andreas.almen@live.com>
2024-08-15 08:55:33 +0000

    add post app and image filter model and serializer

---
+++ posts/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    
+
+    image_filter_choices = [
+    ('action_adventure', 'Action/Adventure fiction'),
+    ('art_photography', 'Art & photography'),
+    ('autobiography_memoir', 'Autobiography/Memoir'),
+    ('biography', 'Biography'),
+    ('childrens_fiction', 'Children’s fiction'),
+    ('classic_fiction', 'Classic fiction'),
+    ('contemporary_fiction', 'Contemporary fiction'),
+    ('essays', 'Essays'),
+    ('fantasy', 'Fantasy'),
+    ('food_drink', 'Food & drink'),
+    ('graphic_novel', 'Graphic novel'),
+    ('historical_fiction', 'Historical fiction'),
+    ('horror', 'Horror'),
+    ('how_to_guides', 'How-To/Guides'),
+    ('humanities_social_sciences', 'Humanities & social sciences'),
+    ('humor', 'Humor'),
+    ('lgbtq', 'LGBTQ+'),
+    ('literary_fiction', 'Literary fiction'),
+    ('mystery', 'Mystery'),
+    ('new_adult', 'New adult'),
+    ('parenting', 'Parenting'),
+    ('philosophy', 'Philosophy'),
+    ('religion_spirituality', 'Religion & spirituality'),
+    ('romance', 'Romance'),
+    ('satire', 'Satire'),
+    ('science_fiction', 'Science fiction'),
+    ('science_technology', 'Science & technology'),
+    ('self_help', 'Self-help'),
+    ('short_story', 'Short story'),
+    ('thriller', 'Thriller'),
+    ('travel', 'Travel'),
+    ('true_crime', 'True crime'),
+    ('western', 'Western'),
+    ('womens_fiction', 'Women’s fiction'),
+    ('young_adult', 'Young adult')
+]
+
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_penf1s', blank=True
+    )
+
+    image_filter =models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
---
+++ posts/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Post
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidateError(
+                'image is larger then 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'image width is larger than 4096 pixels'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'image height is larger than 4096 pixels'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter',
+        ]
