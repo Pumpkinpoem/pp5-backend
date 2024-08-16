ff8db67e5c76c7c51aaace209a7d5d7c26fe53ac
Andreas Almen <andreas.almen@live.com>
2024-08-16 09:34:39 +0000

    add CommentLists and view

---
+++ comments/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics, permissions
+from cup.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from.serializers import CommentSerializer, CommentDetailSerializer
+
+
+
+class CommentList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
